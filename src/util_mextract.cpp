c410be56e5ecbc039dfc897bb74d20f90625c4f9
Andrew Kane <arkane@uwaterloo.ca>
2024-04-14 18:10:09 -0400

    explicitly use std namespace rather than including it

--- src/util_mextract.cpp
+++ src/util_mextract.cpp
@@ -8,33 +8,32 @@
 #include <string>
 #include <set>
 #include <stdio.h>
-using namespace std;
 
 /* read in TREC files, output only specified DOCs */
 
 enum ProcessMode {accum,drop,output};
 
-static inline string mathDOCNO(bool bMath, string s) { if (bMath) { int c=s.find_last_of('_'); if (c>=0) s=s.substr(c+1); } return s; }
+static inline std::string mathDOCNO(bool bMath, std::string s) { if (bMath) { int c=s.find_last_of('_'); if (c>=0) s=s.substr(c+1); } return s; }
 
 static void process(char* fn, bool bMath) {
   // get docs to extract
-  set<string> extract;
-  ifstream in(fn); if (!in) {cerr<<"ERROR: invalid "<<fn<<endl; exit(-1);}
-  for (;;) { string line; getline(in, line); if (!in) break; extract.insert(mathDOCNO(bMath, line)); }
-  cerr<<"Loaded "<<extract.size()<<" DOCNOs for extraction"<<endl;
+  std::set<std::string> extract;
+  std::ifstream in(fn); if (!in) {std::cerr<<"ERROR: invalid "<<fn<<std::endl; exit(-1);}
+  for (;;) { std::string line; getline(in, line); if (!in) break; extract.insert(mathDOCNO(bMath, line)); }
+  std::cerr<<"Loaded "<<extract.size()<<" DOCNOs for extraction"<<std::endl;
   // process with tags from stdin
   int curr=0, size=1<<20; char* buff=(char*)malloc(size); ProcessMode m; int extracted=0, processed=0;
   NEXTDOC:
   m=accum; curr=0;
-  for (;;) { string line; getline(cin, line); if (!cin) goto CLEANUPBUFF;
-    if (line.compare("</DOC>")==0) { processed++; if (m==output) { cout<<line<<endl; extracted++; } goto NEXTDOC; }
+  for (;;) { std::string line; getline(std::cin, line); if (!std::cin) goto CLEANUPBUFF;
+    if (line.compare("</DOC>")==0) { processed++; if (m==output) { std::cout<<line<<std::endl; extracted++; } goto NEXTDOC; }
     else if (m==drop) continue;
-    else if (m==output) { cout<<line<<endl; }
+    else if (m==output) { std::cout<<line<<std::endl; }
     else if (m==accum) {
       if (line.size()>7 && line.compare(0,7,"<DOCNO>")==0) {
         int e=line.find("</DOCNO>",7);
-        if (e>=0) { string docname=mathDOCNO(bMath, line.substr(7,e-7));
-          if (extract.find(docname)!=extract.end()) { m=output; cout.write(buff,curr); curr=0; cout<<line<<endl; continue; }
+        if (e>=0) { std::string docname=mathDOCNO(bMath, line.substr(7,e-7));
+          if (extract.find(docname)!=extract.end()) { m=output; std::cout.write(buff,curr); curr=0; std::cout<<line<<std::endl; continue; }
         }
         m=drop; curr=0; continue; // invalid or non-extract DOCNO, so drop
       }
@@ -43,13 +42,13 @@ static void process(char* fn, bool bMath) {
   }
   CLEANUPBUFF:
   free(buff); buff=NULL;
-  cerr<<"Extracted "<<extracted<<" of "<<processed<<" documents"<<endl;
+  std::cerr<<"Extracted "<<extracted<<" of "<<processed<<" documents"<<std::endl;
 }
 
 int main(int argc, char *argv[]) {
   int s=1; bool bMath=false;
   if (s<argc && strstr(argv[s],"-M")==argv[s] && *(argv[s]+2)==0) { bMath=true; s++; }
-  if (argc-s!=1) { cerr<<"Usage: ./mextract.exe [-M] docno_values.txt < input > output"<<endl; return -1; }
+  if (argc-s!=1) { std::cerr<<"Usage: ./mextract.exe [-M] docno_values.txt < input > output"<<std::endl; return -1; }
   process(argv[s], bMath);
   return 0;
 }
